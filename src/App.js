a31378bce46000a7b192ed0d139eeb080a402e19
Richard Wang <61330312+richdata2020@users.noreply.github.com>
2022-10-16 12:09:26 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="app-container">
       <ExchangeRateForm />
       <ExchangeRateTable />
-      <footer id="footer">Designed by Richard Wang 2020</footer>
+      <footer id="footer">RW-LL-RH Beta 2022</footer>
     </div>
   );
 }
